aa02d9736d857deee71eaac646731e49dcb54740
Hilal Safi <hilalsafi0@gmail.com>
2025-06-21 23:51:06 -0400

    Navbar fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <Navbar />
       <div className="w-full flex-1 flex flex-col items-center justify-center">
         <div className="px-4 py-10 md:py-20">
           <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
@@ -53,33 +51,5 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      {/* <FeatureBentoGrid /> */}
-    </div>
   );
 }
-
-const Navbar = () => {
-  const { user } = useUser();
-  return (
-    <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
-      <div className="flex items-center gap-2">
-        <Image src={'/logo.svg'} alt='logo' width={90} height={90} />
-
-        {/* <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
-        <h1 className="text-base font-bold md:text-2xl">MediVoice AI</h1> */}
-      </div>
-      {!user ?
-        <Link href={'/dashboard'}>
-          <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-            Login
-          </button></Link> :
-        <div className="flex gap-5 items-center">
-          <UserButton />
-          <Link href={'/dashboard'}>
-            <Button>Dashboard</Button>
-          </Link>
-        </div>
-      }
-    </nav>
-  );
-};
